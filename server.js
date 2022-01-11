a9cd63e136ddfb7ffad4492bc6c74e4b00abce60
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-01-10 20:11:06 -0600

    require all packages

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const inquirer = require("inquirer");
+const mysql = require('mysql2/promise');
+const db = require('./db/connection');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  app.listen(PORT, () => {});
+});
