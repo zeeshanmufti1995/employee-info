f1c73c635fc84649c51529e7a5ae5d33de34f013
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-01-12 11:56:27 -0600

    modified file's

--- server.js
+++ server.js
@@ -21,3 +21,113 @@ db.connect(err => {
   if (err) throw err;
   app.listen(PORT, () => {});
 });
+
+
+// Start the prompt functions
+function startPrompt() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee'], 
+
+    }).then( answer => {
+        switch (answer.menu) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                addEmployee();
+                break;
+            case 'Update An Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Update An Employee Manager':
+                updateEmployeeManager();
+                break;
+            case 'Delete Department':
+                deleteDepartment();
+                break;
+            case 'Delete Role':
+                deleteRole();
+                break;
+            case 'Delete Employee':
+                deleteEmployee();
+                break;
+        }
+    })
+ };
+ / View all roles
+function viewAllRoles() {
+    const sql = `SELECT * FROM role`;
+    db.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message })
+            return;
+        }
+        console.table(result);
+        startPrompt();
+    });
+};
+
+// View all employees
+function viewAllEmployees() {
+    const sql = `SELECT employee.id,
+                employee.first_name,
+                employee.last_name,
+                role.title AS job_title,
+                department.department_name,
+                role.salary,
+                CONCAT(manager.first_name, ' ' ,manager.last_name) AS manager
+                FROM employee
+                LEFT JOIN role ON employee.role_id = role.id
+                LEFT JOIN department ON role.department_id = department.id
+                LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+                ORDER By employee.id`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+        startPrompt();
+    });
+};
+
+// Add departments
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: "department_name",
+            type: "input",
+            message: "Please enter the name of the department you want to add to the database."
+        }
+    ]).then((answer) => {
+
+    const sql = `INSERT INTO department (department_name)
+                VALUES (?)`;
+    const params = [answer.department_name];
+    db.query(sql, params, (err, result) => {
+    if (err) throw err;
+    console.log('The new department entered has been added successfully to the database.');
+
+        db.query(`SELECT * FROM department`, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: err.message })
+                return;
+            }
+            console.table(result);
+            startPrompt();
+        });
+    });
+});
+};
